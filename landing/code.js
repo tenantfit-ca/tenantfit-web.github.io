0a0f47541d2946275d5e9c4aaae0e74e8ee59c6e
mtxvp <info@mtxvp.com>
2025-07-27 22:10:45 -0400

    Update UI

--- landing/code.js
+++ landing/code.js
@@ -5,7 +5,8 @@ function saveLead(e) {
     const email = document.getElementById('email').value.trim();
     const agreedToReceiveNews = document.getElementById('agreedToReceiveNews').checked;
     const source = leadForm.querySelector('input[name="leadSource"]').value;
-
+    
+    showOverlay();
     fetch(baseUrl + '/save-prescreening-lead', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
@@ -20,13 +21,15 @@ function saveLead(e) {
     if (data.success) {
         leadForm.style.display = 'none';
         document.getElementById('leadSuccessMsg').style.display = '';
+        showAlert('Email sent to: ' + email);
     } else {
         showAlert(data.message || 'Submission failed. Please try again.', 'danger');
     }
     })
     .catch(() => {
-    showAlert('Submission failed. Please try again.', 'danger');
-    });
+        showAlert('Submission failed. Please try again.', 'danger');
+    })
+    .finally(hideOverlay);
 }
 
 function registerListeners() {
@@ -47,4 +50,16 @@ function showAlert(message, type = 'success') {
     setTimeout(() => {
         alertContainer.classList.add('hidden');
     }, 3000); // Disappear after 3 seconds
+}
+
+function showOverlay() {
+    const overlay = document.getElementById('overlay');
+    overlay.classList.remove('d-none');
+    overlay.classList.add('d-flex');
+}
+
+function hideOverlay() {
+    const overlay = document.getElementById('overlay');
+    overlay.classList.remove('d-flex');
+    overlay.classList.add('d-none');
 }
